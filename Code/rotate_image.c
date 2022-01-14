71e46c7eaddc7337863f8dc58113eb9432e4e691
Gabi-Limberea <gabilimberea@gmail.com>
2022-01-14 23:40:36 +0200

    ROTATE SELECTION DONE for P2/P5 and P3/P6

--- Code/rotate_image.c
+++ Code/rotate_image.c
@@ -6,34 +6,134 @@
 
 #include "rotate_image.h"
 
-int exec_rotation_select_bw()
+int exe_rotation_bw(uint_fast8_t ***matrix, int angle_val,
+							selected_area selected)
 {
+
+	int side = selected.x2 - selected.x1;
+	int rotations = angle_val / DEFAULT_ANGLE;
+
+	if (angle_val >= 0) {
+		while (rotations) {
+			uint_fast8_t **copy = alloc(side, side);
+
+			if (!copy) {
+				free_channel(copy, side);
+				return ERROR;
+			}
+
+			for (int i = 0; i < side; i++)
+				for (int j = 0; j < side; j++)
+					copy[i][j] = (*matrix)[i + selected.x1][j + selected.y1];
+
+			for (int i = selected.x1; i < selected.x2; i++)
+				for (int j = selected.y1; j < selected.y2; j++)
+					swap(&(*matrix)[i][j],
+						 &copy[selected.x2 - j - 1][i - selected.y1]);
+
+			rotations--;
+			free_channel(copy, side);
+		}
+	} else {
+		while(rotations) {
+			uint_fast8_t **copy = alloc(side, side);
+
+			if (!copy) {
+				free_channel(copy, side);
+				return ERROR;
+			}
+
+			for (int i = 0; i < side; i++)
+				for (int j = 0; j < side; j++)
+					copy[i][j] = (*matrix)[i + selected.x1][j + selected.y1];
+
+			for (int i = selected.x1; i < selected.x2; i++)
+					for (int j = selected.y1; j < selected.y2; j++)
+						swap(&(*matrix)[i][j],
+							 &copy[j - selected.x1][selected.y2 - i - 1]);
+
+			rotations++;
+			free_channel(copy, side);
+		}
+	}
+
 	return SUCCESS;
 }
 
-int exec_rotation_select()
+int exe_rotation(uint_fast8_t ***matrix, int angle_val, selected_area selected)
 {
+	int side = selected.x2 - selected.x1;
+	int rotations = angle_val / DEFAULT_ANGLE;
+
+	if (angle_val >= 0) {
+		while (rotations) {
+			uint_fast8_t **copy = alloc(side, side);
+
+			if (!copy) {
+				free_channel(copy, side);
+				return ERROR;
+			}
+
+			for (int i = 0; i < side; i++)
+				for (int j = 0; j < side; j++)
+					copy[i][j] = (*matrix)[i + selected.x1][j + selected.y1];
+
+			for (int i = selected.x1; i < selected.x2; i++)
+				for (int j = selected.y1; j < selected.y2; j++)
+					swap(&(*matrix)[i][j],
+						 &copy[selected.x2 - j - 1][i - selected.y1]);
+
+			rotations--;
+			free_channel(copy, side);
+		}
+	} else {
+		while(rotations) {
+			uint_fast8_t **copy = alloc(side, side);
+
+			if (!copy) {
+				free_channel(copy, side);
+				return ERROR;
+			}
+
+			for (int i = 0; i < side; i++)
+				for (int j = 0; j < side; j++)
+					copy[i][j] = (*matrix)[i + selected.x1][j + selected.y1];
+
+			for (int i = selected.x1; i < selected.x2; i++)
+					for (int j = selected.y1; j < selected.y2; j++)
+						swap(&(*matrix)[i][j],
+							 &copy[j - selected.x1][selected.y2 - i - 1]);
+
+			rotations++;
+			free_channel(copy, side);
+		}
+	}
+
 	return SUCCESS;
 }
 
-int rotate_select(image *img, int angle_value, selected_area *selected)
+int rotate_select(image *img, int angle_val, selected_area selected)
 {
+	if (selected.x2 - selected.x1 != selected.y2 - selected.y1)
+		return ERROR;
+
 	switch (img->type) {
 	case P1:
 	case P4:
-		if (exec_rotation_select_bw() == ERROR)
+		if (exe_rotation_bw(&img->pixels.bw, angle_val, selected) == ERROR)
 			return ERROR;
 		break;
 	case P2:
 	case P5:
-		if (exec_rotation_select() == ERROR)
+		if (exe_rotation(&img->pixels.gray, angle_val, selected) == ERROR)
 			return ERROR;
 		break;
 	case P3:
 	case P6:
-		if (exec_rotation_select() == ERROR ||
-			exec_rotation_select() == ERROR ||
-			exec_rotation_select() == ERROR)
+		if (exe_rotation(&img->pixels.red, angle_val, selected) == ERROR ||
+			exe_rotation(&img->pixels.green, angle_val, selected) == ERROR ||
+			exe_rotation(&img->pixels.blue,
+				angle_val, selected) == ERROR)
 			return ERROR;
 		break;
 	default:
@@ -43,28 +143,28 @@ int rotate_select(image *img, int angle_value, selected_area *selected)
 	return SUCCESS;
 }
 
-int rotate_all(image *img, int angle_value)
+int rotate_all(image *img, int angle_val)
 {
 	switch (img->type) {
 	case P1:
 	case P4:
-		if (exec_rotation__all_bw(&img->pixels.bw, angle_value,
+		if (exe_rotation_all_bw(&img->pixels.bw, angle_val,
 								  &img->width, &img->height) == ERROR)
 			return ERROR;
 		break;
 	case P2:
 	case P5:
-		if (exec_rotation_all(&img->pixels.gray, angle_value,
+		if (exe_rotation_all(&img->pixels.gray, angle_val,
 							  &img->width, &img->height) == ERROR)
 			return ERROR;
 		break;
 	case P3:
 	case P6:
-		if (exec_rotation_all(&img->pixels.red, angle_value,
+		if (exe_rotation_all(&img->pixels.red, angle_val,
 							  &img->width, &img->height) == ERROR ||
-			exec_rotation_all(&img->pixels.green, angle_value,
+			exe_rotation_all(&img->pixels.green, angle_val,
 							  &img->width, &img->height) == ERROR ||
-			exec_rotation_all(&img->pixels.blue, angle_value,
+			exe_rotation_all(&img->pixels.blue, angle_val,
 							  &img->width, &img->height) == ERROR)
 			return ERROR;
 		break;
@@ -75,16 +175,16 @@ int rotate_all(image *img, int angle_value)
 	return SUCCESS;
 }
 
-int exec_rotation_all(uint_fast8_t ***matrix, int angle_value,
+int exe_rotation_all(uint_fast8_t ***matrix, int angle_val,
 					  int *width, int *height)
 {
-	int rotations = angle_value / DEFAULT_ANGLE;
+	int rotations = angle_val / DEFAULT_ANGLE;
 	if (rotations >= 0) {
 		while (rotations) {
 			uint_fast8_t **copy = *matrix;
 			int width_new = *height, height_new = *width;
 
-			*matrix = alloc_channel(width_new, height_new);
+			*matrix = alloc(width_new, height_new);
 			if (!(*matrix))
 				return ERROR;
 
@@ -102,7 +202,7 @@ int exec_rotation_all(uint_fast8_t ***matrix, int angle_value,
 			uint_fast8_t **copy = *matrix;
 			int width_new = *height, height_new = *width;
 
-			*matrix = alloc_channel(width_new, height_new);
+			*matrix = alloc(width_new, height_new);
 			if (!(*matrix))
 				return ERROR;
 
@@ -119,9 +219,54 @@ int exec_rotation_all(uint_fast8_t ***matrix, int angle_value,
 	return SUCCESS;
 }
 
-int exec_rotation_all_bw(uint_fast8_t ***matrix, int angle_value,
+int exe_rotation_all_bw(uint_fast8_t ***matrix, int angle_val,
 						 int *width, int *height)
 {
+	int rotations = angle_val / DEFAULT_ANGLE;
+
+	if (rotations >= 0) {
+		while (rotations) {
+			uint_fast8_t **copy = *matrix;
+			int width_new = *height, height_new = *width;
+
+			*matrix = alloc(width_new, height_new);
+			if (!(*matrix))
+				return ERROR;
+
+			for (int i = 0; i < height_new; i++) {
+				for (int j = 0; j < width_new; j++) {
+					if (is_bit_set(copy, *height - j - 1, i))
+						set_bit(1, *matrix, i, j);
+				}
+			}
+
+			free_channel(copy, *height);
+			rotations--;
+			*width = width_new;
+			*height = height_new;
+		}
+	} else {
+		while (rotations) {
+			uint_fast8_t **copy = *matrix;
+			int width_new = *height, height_new = *width;
+
+			*matrix = alloc(width_new, height_new);
+			if (!(*matrix))
+				return ERROR;
+
+			for (int i = 0; i < height_new; i++) {
+				for (int j = 0; j < width_new; j++) {
+					if (is_bit_set(copy, j, *width - i - 1))
+						set_bit(1, *matrix, i, j);
+				}
+			}
+
+			free_channel(copy, *height);
+			rotations++;
+			*width = width_new;
+			*height = height_new;
+		}
+	}
 	return SUCCESS;
 }
 
@@ -133,15 +278,15 @@ void rotate(status *img_status, char *angle, image *img,
 		return;
 	}
 
-	int angle_value = atoi(angle);
-	if (angle_value % DEFAULT_ANGLE != 0 || angle_value > DEFAULT_ANGLE * 4 ||
-		angle_value < DEFAULT_ANGLE * (-4)) {
+	int angle_val = atoi(angle);
+	if (angle_val % DEFAULT_ANGLE != 0 || angle_val > DEFAULT_ANGLE * 4 ||
+		angle_val < DEFAULT_ANGLE * (-4)) {
 		printf(ROTATE_FAIL_ANGlE);
 		return;
 	}
 
 	if (img_status->selection == SELECTED_ALL) {
-		if (rotate_all(img, angle_value) == SUCCESS)
+		if (rotate_all(img, angle_val) == SUCCESS)
 			printf(ROTATE_OK, angle);
 		else
 			fprintf(stderr, ERROR_MESSAGE);
@@ -149,12 +294,9 @@ void rotate(status *img_status, char *angle, image *img,
 	}
 
 	if (selected->x2 - selected->x1 == selected->y2 - selected->y1) {
-		if (rotate_select(img, angle_value, selected) == SUCCESS)
+		if (rotate_select(img, angle_val, *selected) == SUCCESS)
 			printf(ROTATE_OK, angle);
-		else
-			fprintf(stderr, ERROR_MESSAGE);
 		return;
 	}
-
 	printf(ROTATE_FAIL_SQUARE);
 }
